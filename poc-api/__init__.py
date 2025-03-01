c5cfed44cb5309895f7c105a8b749f24bee8fa80
Minh Hikari <minhhikari1994+dev@outlook.com>
2025-03-01 16:26:48 +0700

    add sentry and example .env

--- poc-api/__init__.py
+++ poc-api/__init__.py
@@ -1,4 +1,4 @@
-import os
+import os, sentry_sdk
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -7,9 +7,30 @@ from .models.base import db, migrate
 from .helpers.auth_helper import login_manager
 from .blueprints import register_blueprints
 
+def init_sentry():
+    sentry_sdk.init(
+        dsn=os.environ.get('SENTRY_DSN'),
+        # Add data like request headers and IP for users,
+        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+        send_default_pii=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        traces_sample_rate=1.0,
+        _experiments={
+            # Set continuous_profiling_auto_start to True
+            # to automatically start the profiler on when
+            # possible.
+            "continuous_profiling_auto_start": True,
+        },
+    )
+
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
     load_dotenv()
+
+    if os.environ.get('SENTRY_DSN'):
+        init_sentry()
+    
+    app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
         SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI'),
